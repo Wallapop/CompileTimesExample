ebdf3e79c3ca5a09fc5e7e2eda02caf20deb39b1
Luis Ascorbe <devlascorbe@gmail.com>
2017-03-15 19:36:05 +0100

    Add Xcode project

---
+++ CompileTimesExample/ViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  CompileTimesExample
+//
+//  Created by Luis Ascorbe on 15/03/2017.
+//  Copyright © 2017 Wallapop. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+}
